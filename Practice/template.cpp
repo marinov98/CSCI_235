5bedfbebe46f56e9a17cd57292aa4b3aeec4499c
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-09-22 14:01:38 -0400

    some new tricks added

--- Practice/template.cpp
+++ Practice/template.cpp
@@ -8,12 +8,32 @@ void Print(T value)
 	std::cout << value << '\n';
 }
 
+// Array example
+template <typename T, int N>
+class Array
+{
+  private:
+	T m_Array[N];
+
+  public:
+	int GetSize() const
+	{
+		return N;
+	}
+};
+
 int main()
 {
+	// Print example
 	Print(5);
 	Print(24.25);
 	Print(44.12345);
 	Print("Template mothertrucker");
-	/* code */
+
+	// Array example
+	// Array<int, 25> array;
+	Array<std::string, 50> array;
+	std::cout << array.GetSize() << '\n';
+
 	return 0;
 }
