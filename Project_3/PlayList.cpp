107bae1054e5fc134cd5bf1f790f13a927495c46
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-10-14 20:39:15 -0400

    better

--- Project_3/PlayList.cpp
+++ Project_3/PlayList.cpp
@@ -16,38 +16,7 @@ PlayList::PlayList(const Song& a_song) {
 }
 
 PlayList::PlayList(const PlayList& a_play_list) : LinkedSet<Song>(a_play_list) {
-    item_count_ = a_play_list.item_count_;
-    Node<Song>* orig_chain_ptr = a_play_list.head_ptr_; // Points to nodes in original chain
-
-    if (orig_chain_ptr == nullptr) {
-        head_ptr_ = nullptr; // Original Set is empty
-        tail_ptr_ = nullptr; // tail and head are the same when set is empty
-    }
-    else {
-        // Copy first node
-        head_ptr_ = new Node<Song>();
-        head_ptr_->setItem(orig_chain_ptr->getItem());
-
-        // Copy remaining nodes
-        Node<Song>* new_chain_ptr = head_ptr_;  // Points to last node in new chain
-        orig_chain_ptr = orig_chain_ptr->getNext(); // Advance original-chain pointer
-
-        while (orig_chain_ptr != nullptr) {
-            // Get next item from original chain
-            Song nextItem = orig_chain_ptr->getItem();
-            // Create a new node containing the next item
-            auto new_node_ptr = new Node<Song>(nextItem);
-            // Link new node to end of new chain
-            new_chain_ptr->setNext(new_node_ptr);
-            // Advance pointer to new last node
-            new_chain_ptr = new_chain_ptr->getNext();
-            // Advance original-chain pointer
-            orig_chain_ptr = orig_chain_ptr->getNext();
-        } // end while
-
-        new_chain_ptr->setNext(nullptr); // Flag end of chain
-        tail_ptr_ = getPointerToLastNode(); // make sure the tail pointer points to the last node;
-    }
+    tail_ptr_ = getPointerToLastNode();
 } // copy constructor
 
 // destructor
