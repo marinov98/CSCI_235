2bd718901a1e8ea71a087e18f0cdab30762ea0d5
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-11-01 17:19:14 -0400

    Beginning project 4

--- Project_4/MazeSolver.cpp
+++ Project_4/MazeSolver.cpp
@@ -3,3 +3,55 @@
 // Date: 11/01/18
 // File Name: MazeSolver.cpp
 // This file implements the MazeSolver class
+
+#include "MazeSolver.h"
+// constructor
+MazeSolver::MazeSolver(std::string input_file) {
+
+}
+
+// destructor
+MazeSolver::~MazeSolver() {
+
+}
+// public functions
+bool MazeSolver::mazeIsReady() {
+
+}
+
+bool  MazeSolver::solveMaze() {
+
+}
+
+void MazeSolver::printSolution() {
+
+}
+
+// private helper functions
+void MazeSolver::initializeMaze(int rows, int columns) {
+
+}
+
+void MazeSolver::fillMaze(std::ifstream &input_stream) {
+
+}
+
+void MazeSolver::initializeSolution() {
+
+}
+
+void MazeSolver::copyMazetoSolution() {
+
+}
+
+bool MazeSolver::extendPath(Position current_position) {
+
+}
+
+Position MazeSolver::getNewPosition(Position old_position, direction dir) {
+
+}
+
+bool MazeSolver::isExtensible(Position current_position, direction dir) {
+
+}
