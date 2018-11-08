33701babb617ddc3b474a774dc03a1c2c87a8dfe
Marin Pavlinov Marinov <wbbmarin5@gmail.com>
2018-11-08 17:23:53 -0500

    Beginning project 5

---
+++ Project_5/SortingTests.cpp
@@ -0,0 +1,28 @@
+// Assignment: CSCI 235, Fall 2018, Project 5
+// Name: Marin Pavlinov Marinov
+// Date: 11/08/18
+// File Name: Sortingtests.cpp
+// This file implements the MazeSolver class
+
+#include "SortingTests.hpp"
+
+
+void selectionSort(int a[], size_t size) {}
+
+void insertionSort(int a[], size_t size) {}
+
+void mergeSort(int a[], size_t size) {}
+
+void generateRandomArray(int values[], size_t size) {}
+
+void generateAscendingArray(int values[], size_t size) {}
+
+void generateDescendingArray(int values[], size_t size) {}
+
+void generateLastTenRandomArray(int values[], size_t size) {}
+
+void generateFewRandomArray(int values[], size_t size) {}
+
+double sortTest(void (*sortingFunction)(int a[], size_t size), int values[], size_t size) {}
+
+double mergeSortTest(int valuess[], size_t size) {}
---
+++ Project_5/main.cpp
@@ -0,0 +1,14 @@
+#include "SortingTests.hpp"
+
+int main(int argc, char *argv[]) {
+    
+    // generator tests 
+
+    // mergesort
+
+    // selection sort
+
+    // insertion sort
+    
+    return 0;
+}
