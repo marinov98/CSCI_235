0f87b4839e0eab30eb0d5d03362e2f273dd19e87
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2018-12-22 13:22:30 -0500

    trying out vector::insert

---
+++ DataStrucutures/VectorInsert.cpp
@@ -0,0 +1,27 @@
+int main()
+{
+  std::vector<int> myvector (5,25);
+  std::vector<int>::iterator it;
+
+  it = myvector.end();
+  myvector.insert(it,50);
+  //it = myvector.insert ( it , 200 );
+
+ // myvector.insert (it,2,300);
+
+  // "it" no longer valid, get a new one:
+//   it = myvector.begin();
+
+//   std::vector<int> anothervector (2,400);
+//   myvector.insert (it+2,anothervector.begin(),anothervector.end());
+
+//   int myarray [] = { 501,502,503 };
+//   myvector.insert (myvector.begin(), myarray, myarray+3);
+
+  std::cout << "myvector contains:";
+  for (it=myvector.begin(); it<myvector.end(); it++)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
+}
